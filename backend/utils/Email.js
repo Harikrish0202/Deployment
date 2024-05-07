e7af2ea059c070f80a78d1ed0120679e8b193575
Harikrishnan <141903355+Harikrish0202@users.noreply.github.com>
2024-05-07 17:16:51 +0530

    Update Email.js

--- backend/utils/Email.js
+++ backend/utils/Email.js
@@ -1,41 +1,42 @@
 const nodemailer = require("nodemailer");
 const sendGridMail = require("@sendgrid/mail");
+
 sendGridMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendEmail = async (options) => {
-  // Define mailOptions
   let mailOptions = {
-    from: process.env.EMAIL_FROM, // Use EMAIL_FROM for sender email
+    from: "test@test.com", // Sender email
     to: options.email,
     subject: options.subject,
     html: options.message,
   };
 
-  // Create a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
-
-  // Send email using transporter
-  await transporter.sendMail(mailOptions);
-
   if (process.env.NODE_ENV === "production") {
     // Modify mailOptions for production environment
     mailOptions.from = {
       name: "Homly hub",
-      email: process.env.EMAIL_FROM, // Use EMAIL_FROM for sender email
+      email: process.env.SENDGRID_EMAIL,
     };
+    // Send email using SendGrid
     try {
+      console.log(mailOptions);
       await sendGridMail.send(mailOptions);
       console.log("Email sent successfully!");
     } catch (error) {
       console.error("Error sending email:", error);
     }
+  } else {
+    // Create a transporter for non-production environment
+    const transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+    // Send email using transporter
+    await transporter.sendMail(mailOptions);
   }
 };
 
